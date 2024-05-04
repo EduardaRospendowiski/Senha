61b5cede987327d1f84505f4472a7d9f3f3c416f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:58:04 -0300

    atividade completa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+const string senhaCorreta = "1234abcd";
+string senhaGeral; 
+
+Console.Write("ola usuario, digite sua senha: ");
+senhaGeral = Console.ReadLine()!;
+
+if(senhaGeral == senhaCorreta)
+{
+    Console.WriteLine("Acesso permitido");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
+}
+
